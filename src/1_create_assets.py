66ad2ef8e0af2bda569bd1663dd192cbb9f9ec9d
lance <lance@qq.com>
2024-12-10 20:29:27 +0800

    create 4 assets

---
+++ src/1_create_assets.py
@@ -0,0 +1,60 @@
+import syft as sy
+import pandas as pd
+import numpy as np
+import torch
+
+SEED = 2024
+np.random.seed(SEED)
+
+SLIDE_DATA = pd.read_csv(f'src/data.csv', index_col=0)
+
+
+for idx in range(4):
+    data_site = sy.orchestra.launch(
+        name=f"gleason-research-centre-{idx}", reset=True)
+    dataOwner = data_site.login(
+        email="info@openmined.org", password="changethis")
+
+    data = SLIDE_DATA.loc[:, f'client{idx}'].dropna()
+    featuresArr = []
+    featuresArr_mock = []
+    labelArr = []
+    labelArr_mock = []
+    for id in data:
+        insE = torch.load(
+            f'dataset/efficientnet_b0/{id}.pth', weights_only=False)
+        insM = torch.load(
+            f'dataset/MambaVision-S-1K/{id}.pth', weights_only=False)
+        features1 = torch.transpose(insE['features'], 1, 0)
+        features2 = torch.transpose(insM['features'], 1, 0)
+        features = torch.cat((features1, features2), dim=2)
+        featuresArr.append(features)
+        featuresArr_mock.append(torch.from_numpy(
+            np.random.uniform(low=-1, size=features.shape)))
+        labelArr.append(insE['label'])
+        labelArr_mock.append([0, 1, 1, 0])
+
+    features_asset = sy.Asset(
+        name="Gleason Cancer Data: Features",
+        data=featuresArr,      # real data
+        mock=featuresArr_mock  # mock data
+    )
+    targets_asset = sy.Asset(
+        name="Gleason Cancer Data: Targets",
+        data=labelArr,      # real data
+        mock=labelArr_mock  # mock data
+    )
+
+    gleason_cancer_dataset = sy.Dataset(
+        name="Gleason Cancer Biomarker",
+        description='',
+        summary='',
+        citation='',
+        url='',
+    )
+    gleason_cancer_dataset.add_asset(features_asset)
+    gleason_cancer_dataset.add_asset(targets_asset)
+
+    dataOwner.upload_dataset(dataset=gleason_cancer_dataset)
+
+    data_site.land()
